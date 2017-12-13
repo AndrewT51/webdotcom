c127a94a7bfc5c9f65ed483435878f642a2d9ba3
Andrew Trigg <andrewt51@hotmail.com>
2017-12-13 19:44:01 +0000

    Add morgan library to log requests

--- app.js
+++ app.js
@@ -1,13 +1,24 @@
 require('dotenv').config();
 require('./db');
+
+const fs = require('fs');
+const path = require('path');
 const { PORT } = process.env;
+
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const routes = require('./routes');
 
+// set up morgan using examples from npm page. Sets up logging file in append mode
+const logger = require('morgan');
+const logDirectory = path.join(__dirname, 'logs')
+fs.existsSync(logDirectory) || fs.mkdirSync(logDirectory)
+const accessLogStream = fs.createWriteStream(path.join(logDirectory, 'access.log'), { flags: 'a'});
+
 // Middleware
 app.use(bodyParser.json());
+app.use(logger('combined', { stream: accessLogStream }));
 
 // Routes
 app.use(routes);
@@ -27,4 +38,6 @@ app.use((err, req, res, next) => {
   res.send(err.message);
 });
 
+
+// begin listening for requests
 app.listen(PORT, () => console.log(`Started on port ${ PORT }`));
