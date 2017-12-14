960e90e9865f0f80ee51b2fe1b99281bf88a6a01
Andrew Trigg <andrewt51@hotmail.com>
2017-12-14 12:52:12 +0000

    Ensure the database connection result isn't shown when running tests

--- db/index.js
+++ db/index.js
@@ -1,11 +1,11 @@
 require('dotenv').config()
 require('../models');
-const { MONGO_URL, TEST_MONGO_URL } = process.env
-const DB_URL = process.env.NODE_ENV === 'test' ? TEST_MONGO_URL : MONGO_URL
+const { MONGO_URL, TEST_MONGO_URL, NODE_ENV } = process.env
+const DB_URL = NODE_ENV === 'test' ? TEST_MONGO_URL : MONGO_URL
 const mongoose = require('mongoose');
 const Promise = require('bluebird');
 mongoose.Promise = Promise;
 
 mongoose.connect(DB_URL, { useMongoClient: true })
-.then( () => console.log(`Connected to database: ${DB_URL}`))
+.then( () => NODE_ENV !== 'test' && console.log(`Connected to database: ${DB_URL}`))
 .catch( err => console.log(`Error connecting to database: ${err}`))
